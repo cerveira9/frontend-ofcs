f71cac8a7fa785fc4f335b978a9f549cb145af7c
Fernando Cerveira <cerveira9@gmail.com>
2025-05-07 15:35:42 -0300

    refactor: changed api to get better env url

--- src/api.js
+++ src/api.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:
-    import.meta.env.MODE === 'development'
-      ? 'http://localhost:5000/v1/api'
-      : 'https://be-oficiais.onrender.com/v1/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export default api;
